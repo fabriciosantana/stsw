d60773a0ce2feb4b06e900b4f57a87131b09b1d1
jtavora <jtavora09@gmail.com>
2024-08-14 20:53:16 -0300

    Ex1

--- ex01/Triangulo.py
+++ ex01/Triangulo.py
@@ -1,11 +1,25 @@
 class Triangulo():
     def __init__(self, a, b, c):
-        print("Implementar classe")
+        self.a = a
+        self.b = b
+        self.c = c
 
     def tipo_triangulo(self):
-
-        if self.eh_valido():
+        self.eh_valido()
+        
+        if self.a == self.b == self.c:
             return "Equilátero"
+        elif self.a == self.b or self.a == self.c or self.b == self.c:
+            return "Isósceles"
+        else:
+            return "Escaleno"
     
     def eh_valido(self):
+        if self.a + self.b <= self.c or self.a + self.c <= self.b or self.b + self.c <= self.a:
+            raise Exception("Os lados informados não formam um triângulo")
+        elif self.a <= 0 or self.b <= 0 or self.c <= 0:
+            raise Exception("Os lados informados devem ser maiores que zero")
+        elif self.a > 200 or self.b > 200 or self.c > 200 or self.a < 1 or self.b < 1 or self.c < 1:
+            raise Exception("Os lados informados não podem ser nulos")
+        
         return True
--- ex01/app.py
+++ ex01/app.py
@@ -2,9 +2,15 @@ from Triangulo import Triangulo
 
 def main():
     try:
-        print("Implementar programa do triângulo")
-    except Exception:
-        print("Ocorreu um erro")
+        a = int(input("Informe o lado A: "))
+        b = int(input("Informe o lado B: "))
+        c = int(input("Informe o lado C: "))
+        
+        triangulo = Triangulo(a, b, c)
+        print(triangulo.tipo_triangulo())
+
+    except Exception as e:
+        print(e)
 
 if __name__ == "__main__":
     main()
