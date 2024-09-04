93a0b7d50f4b109bb22bb3bf62fac0a6d64db374
fabriciosantana <fabricio.santana@gmail.com>
2024-09-04 10:18:28 -0300

    alguns testes unitários com pytest

--- ex01/pytests/Triangulo_test.py
+++ ex01/pytests/Triangulo_test.py
@@ -1,4 +1,5 @@
 from Triangulo import Triangulo
+import pytest
 
 class TestTriangulo():
     
@@ -11,11 +12,31 @@ class TestTriangulo():
         assert t.tipo_triangulo() == "Escaleno"
         t = Triangulo(1,2,3)
         assert t.tipo_triangulo() == "Não é um triângulo"
+        t = Triangulo(201,2,3)
+        assert t.tipo_triangulo() == "Fora do intervalo"
     
     def test_eh_valido(self):
         t = Triangulo(1,1,1)
         assert t.eh_valido() == True
+        t = Triangulo(1,1,3)
+        assert t.eh_valido() == False
     
     def test_eh_valido_intervalo_dos_lados(self):
         t = Triangulo(1,1,1)
         assert t.eh_valido_intervalo_dos_lados() == True
+        t = Triangulo(201,1,1)
+        assert t.eh_valido_intervalo_dos_lados() == False
+        
+        try:
+            t = Triangulo("a",1,1)
+            assert False
+        except ValueError:
+            assert True
+        
+
+    @pytest.mark.parametrize("a,b,c,expected", [(1,1,1, True)])
+    def test_eh_valido2(self, a, b, c, expected):
+        t = Triangulo(a,b,c)
+        assert t.eh_valido() == expected
+
+
